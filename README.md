1ee4f6ab2c913c6b4ad990f3a8288087d2c4305c
Andy Pearson <andy@londonparkour.com>
2020-05-25 19:33:28 +0100

    Add emoji description

---
+++ README.md
@@ -0,0 +1,4 @@
+# CLASSES From Teambooking - Sitemap
+
+The creates a custom sitemap file classschema-sitemap.xml that adds all upcoming class to it.
+This means google can pick up the classes and add to its index.
--- teambooking_rankmath_schema.php
+++ teambooking_rankmath_schema.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: _ANDYP - Team Booking - RankMath Schema Sitemap
 Plugin URI: https://londonparkour.com
-Description: This will create a new taxonomy called classschema that can be used as a sitemap for RankMath. This sitemap then has all classes added to it from TeamBooking
+Description: <strong>🌍SITEMAP</strong> | <em>Posts > Class Schema</em> | This will create a new taxonomy called classschema that can be used as a sitemap for RankMath. This sitemap then has all classes added to it from TeamBooking
 Version: 1.0.1
 Author: Andy Pearson
 Author URI: https://londonparkour.com
